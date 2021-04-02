9c6f122cba591e776f13e198a83d560f629dc7d7
DESKTOP-8NK97U4\patyg <giulianoantonanzas@gmail.com>
2021-04-02 10:58:46 -0300

    Creaccion de Landing Page
    La pagina ya tiene navbar, masthead y una seccion. Falta agregar las demas secciones.

---
+++ src/components/About/About.js
@@ -0,0 +1,30 @@
+import './Style.scss'
+
+
+const About = () => {
+    return (
+        <div className="section2" id="about">
+            <div className="content-s2" >
+                <div className="container">
+                    <p className="text1-s2"> We need Businesses 
+                    <p className="text1span-s2"> Grow and Innovate</p>
+                    </p>
+                    <p className="text2-s2">
+                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+                        sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+                        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+                        Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+                        sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+                        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+                        Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+                        sed diam nonumy eirmod tempor invidunt ut
+                </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,57 @@
+import './Style.scss'
+import logo from '../../assets/images/top-logo.svg'
+
+
+
+const Navbar = () => {
+
+    const displayMenu= () =>{
+        let button= document.getElementsByClassName("mobile-menu")
+        button[0].classList.toggle("change");
+        let x = document.getElementById("nav");
+        if (x.className === "nav") {
+            x.className += " responsive";
+        } else {
+            x.className = "nav";
+        }
+    }
+
+
+    window.onscroll = () => {
+        if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+            document.getElementById("nav").style.marginBottom = "-8px";
+            document.getElementById("nav").style.marginTop = "-8px";
+            document.getElementById("nav").style.transition = "0.4s";
+            document.getElementById("nav").style.backgroundColor = "rgba(0, 0, 0, 0.212)";
+        } else {
+            document.getElementById("nav").style.margin = "0px 0px";
+            document.getElementById("nav").style.transition = "0.4s";
+            document.getElementById("nav").style.backgroundColor = "transparent";
+        }
+    }
+
+
+    return (
+        <header>
+            <nav className="nav" id="nav">
+                <ul className="nav-conteiner">
+                    <li className="nav-logo mr-auto p-2 bd-highlight"><a href="#home"><img src={logo} alt='logo 25Watts'/></a></li>
+                    <div class="mobile-menu" onClick={()=>displayMenu()}>
+                        <div class="bar1"></div>
+                        <div class="bar2"></div>
+                        <div class="bar3"></div>
+                    </div>
+                    <div className="navLinks" id="navLinks">
+                        <li className="nav-element"><a href="#home">Home</a></li>
+                        <li className="nav-element"><a href="#about">About</a></li>
+                        <li className="nav-element"><a href="#product">Products</a></li>
+                        <li className="nav-element"><a href="#services">Services</a></li>
+                        <li className="nav-element"><a href="#contact">Contact</a></li>
+                    </div>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar
